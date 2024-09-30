109ecd333c97f95e97cb6b2cd1c1f30029e592e0
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-09-30 13:07:29 +0530

    test fix

---
+++ about_script.js
@@ -0,0 +1,6 @@
+// Set the logo source dynamically based on the current location
+document.addEventListener('DOMContentLoaded', () => {
+    const logoImage = document.getElementById('vrishab');
+    // Directly using the relative path
+    logoImage.src = 'photos/logo.jpeg'; // Update this path if necessary
+});
