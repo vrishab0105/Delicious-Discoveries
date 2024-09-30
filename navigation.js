5afe7719ad4857908f315b43d03b8f874730c2bc
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-09-30 12:39:16 +0530

    logo bug fix

--- navigation.js
+++ navigation.js
@@ -2,3 +2,11 @@
 window.navigateTo = function(page) {
     window.location.href = page;
 };
+
+
+// Set the logo source dynamically based on the current location
+document.addEventListener('DOMContentLoaded', () => {
+    const logoImage = document.getElementById('navbar-logo');
+    const baseUrl = window.location.origin; // Get the base URL
+    logoImage.src = `${baseUrl}/path/to/your/Logo.jpeg`; // Set the path to your logo
+});
