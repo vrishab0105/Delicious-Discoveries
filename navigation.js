29d716a283ad5e9d46183b6e487927a05f434313
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-09-30 12:51:21 +0530

    test

--- navigation.js
+++ navigation.js
@@ -3,10 +3,10 @@ window.navigateTo = function(page) {
     window.location.href = page;
 };
 
-
 // Set the logo source dynamically based on the current location
 document.addEventListener('DOMContentLoaded', () => {
     const logoImage = document.getElementById('navbar-logo');
     const baseUrl = window.location.origin; // Get the base URL
-    logoImage.src = `${baseUrl}/Logo.jpeg`;
+    // Adjust the path based on where your image is located
+    logoImage.src = `${baseUrl}/Logo.jpeg`; // Update this path if necessary
 });
