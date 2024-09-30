007c562ee4e8e0ea9604c713a190063881ce4a02
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-09-30 13:15:23 +0530

    test fix

--- about_script.js
+++ about_script.js
@@ -1,6 +1,18 @@
-// Set the logo source dynamically based on the current location
+// about_script.js
+
 document.addEventListener('DOMContentLoaded', () => {
-    const logoImage = document.getElementById('vrishab');
-    // Directly using the relative path
-    logoImage.src = 'photos/vrishab.jpeg'; // Update this path if necessary
+
+    // Set sources for team members' photos
+    const student1Photo = document.getElementById('student1-photo');
+    student1Photo.src = 'photos/Ganesh.jpeg'; // Update the path as necessary
+
+    const student2Photo = document.getElementById('student2-photo');
+    student2Photo.src = 'photos/Vrishab.jpeg'; // Ensure the file name and path are correct
+
+    const student3Photo = document.getElementById('student3-photo');
+    student3Photo.src = 'photos/Sushant.jpeg'; // Update the path as necessary
+
+    // Set source for special thanks photo
+    const specialThanksPhoto = document.getElementById('special-thanks-photo');
+    specialThanksPhoto.src = 'photos/Armaan2.jpeg'; // Update the path as necessary
 });
