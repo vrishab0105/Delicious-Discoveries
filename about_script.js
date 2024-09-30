c7fcc9b3e7e96b6b476c35fdab0be50f36420da4
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2024-09-30 13:19:55 +0530

    test fix

--- about_script.js
+++ about_script.js
@@ -4,15 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Set sources for team members' photos
     const student1Photo = document.getElementById('student1-photo');
-    student1Photo.src = 'photos/Ganesh.jpeg'; // Update the path as necessary
+    student1Photo.src = 'photos/ganesh.jpeg'; // Update the path as necessary
 
     const student2Photo = document.getElementById('student2-photo');
-    student2Photo.src = 'photos/Vrishab.jpeg'; // Ensure the file name and path are correct
+    student2Photo.src = 'photos/vrishab.jpeg'; // Ensure the file name and path are correct
 
     const student3Photo = document.getElementById('student3-photo');
-    student3Photo.src = 'photos/Sushant.jpeg'; // Update the path as necessary
+    student3Photo.src = 'photos/sushant.jpeg'; // Update the path as necessary
 
     // Set source for special thanks photo
     const specialThanksPhoto = document.getElementById('special-thanks-photo');
-    specialThanksPhoto.src = 'photos/Armaan2.jpeg'; // Update the path as necessary
+    specialThanksPhoto.src = 'photos/armaan.jpeg'; // Update the path as necessary
 });
